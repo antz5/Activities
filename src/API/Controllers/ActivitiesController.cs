9d84f5b07c5f45ece97e199889b1d661233a78bf
ananth <ananthsmurthy@gmail.com>
2021-09-23 15:27:15 +1000

    Added cqrs using mediatr library

--- src/API/Controllers/ActivitiesController.cs
+++ src/API/Controllers/ActivitiesController.cs
@@ -2,36 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
-namespace API.Controllers 
-{    
-   [ApiVersion("1.0")]
+namespace API.Controllers {
+    [ApiVersion ("1.0")]
     [ApiController]
-    [Route("activity/v{apiVersion:apiVersion}/[controller]")]
-    public class ActivitiesController : ControllerBase 
-    {
-        private readonly DataContext _context;
-        public ActivitiesController (DataContext context)
-         {
-            _context = context;
-         }
+    [Route ("activity/v{apiVersion:apiVersion}/[controller]")]
+    public class ActivitiesController : BaseApiController {       
 
-         [HttpGet]
-         public async Task<ActionResult<List<Activity>>> GetActivities()
-         {
-             var activities = await _context.Activities.ToListAsync();
-             return activities;
-         } 
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities () {
+            var activities = await Mediator.Send(new List.Query());
+            return activities;
+        }
 
-         [HttpGet("{Id}")]
-         public async Task<ActionResult<Activity>> GetActivity(Guid Id)
-         {
-             var activity = await _context.Activities.FindAsync(Id);
-             return activity;
-         }
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity (Guid id) {
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity([FromBody] Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateActivity(Guid id,[FromBody]Activity activity){
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
     }
 }
--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -1,3 +1,5 @@
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -28,6 +30,7 @@ namespace API {
             });
 
             services.AddApiVersioning();
+            services.AddMediatR(typeof(List.Handler).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
