8de0176fb88704b880233e0323586d142a9e326c
ananth <ananthsmurthy@gmail.com>
2021-09-25 00:12:47 +1000

    Add aspnet core Identity Registration of Identity services Seeding of user data

---
+++ src/API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection RegisterApplicationServices (this IServiceCollection services, 
+        IConfiguration config)
+        {
+             services.AddControllers ();
+            services.AddSwaggerGen (c => {
+                c.SwaggerDoc ("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+
+            services.AddDbContext<DataContext> (opt => {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddApiVersioning();
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
